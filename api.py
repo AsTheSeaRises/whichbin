1a643b5274023277b6d806b58377f7108e763fca
FuzzyGorilla <asthesearises@gmail.com>
2022-11-15 17:17:56 +0000

    Add files via upload

---
+++ api.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+bin = {"21Nov": "recycling-black bin", "28Nov" : "rubbish-green", "5Dec" : "recycling-glass"}
+
+class HelloWorld(Resource):
+        def get(self, name):
+            return bin[name]
+api.add_resource(HelloWorld, "/whichbin/<string:name>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
